854fbd82f82133244e4c84f0229016da430e2b09
Dinis <dinisvferreira@gmail.com>
2024-04-26 10:28:38 +0100

    add improvement

--- PlayerManager1/Program.cs
+++ PlayerManager1/Program.cs
@@ -23,8 +23,44 @@ namespace PlayerManager2
         {
             playerList = new List<Player>()
             {
-                new Player("Kratos", 6969),
-                new Player("Drake", 4200)
+                new Player("luffy", 6969),
+                new Player("pikachu", 4200)
             };
         }
+
+        public void Start()
+        {
+            string option;
+
+            do
+            {
+                ShowMenu();
+                option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        InsertPlayer();
+                        break;
+                    case "2":
+                        ListPlayers();
+                        break;
+                    case "3":
+                        ListPlayersWithScoreGreaterThan();
+                        break;
+                    case "4":
+                        Console.WriteLine("Bye!");
+                        break;
+                    default:
+                        Console.Error.WriteLine("\n>>> Unknown option! <<<\n");
+                        break;
+                }
+
+                Console.Write("\nPress any key to continue...");
+                Console.ReadKey(true);
+                Console.WriteLine("\n");
+
+            } while (option != "4");
+        }
+
 
