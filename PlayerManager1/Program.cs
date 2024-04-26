1d3d2146aafcf4e079dd06aa7e3a6236479cbbb5
Dinis <dinisvferreira@gmail.com>
2024-04-26 10:24:57 +0100

    add classes

---
+++ PlayerManager1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayerManager2
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+
+    public class PlayerManager
+    {
+        private List<Player> playerList;
+
+        public PlayerManager()
+        {
+            playerList = new List<Player>()
+            {
+                new Player("Kratos", 6969),
+                new Player("Drake", 4200)
+            };
+        }
+
